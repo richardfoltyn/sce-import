c97d47c4c963e0e89dd13cbc8cb84b6fe9cfb896
Richard Foltyn <richard.foltyn@gmail.com>
2025-08-08 20:08:41 +0200

    Restructure main script, fix sample period for replicability

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
+import datetime
 import hashlib
 import logging
 import os.path
+from typing import Optional
 
 import pandas as pd
 
@@ -15,6 +17,63 @@ def md5sum(file_path):
         for chunk in iter(lambda: f.read(4096 * 4), b""):
             hash_md5.update(chunk)
     return hash_md5.hexdigest()
+
+
+def process_data(
+    df_orig: pd.DataFrame,
+    df_ranks: Optional[pd.DataFrame] = None,
+    final_date: Optional[datetime.date] = None,
+) -> tuple[pd.DataFrame, pd.DataFrame]:
+    """
+    Precess SCE raw data and return a full data set and a reduced extract.
+
+    Parameters
+    ----------
+    df_orig : pd.DataFrame
+        Original SCE data, concatenated into single DataFrame.
+    df_ranks : pd.DataFrame, optional
+        Mapping of family income in USD to income ranks
+    final_date : datetime.date, optional
+        If not None, restrict sample to dates before and including this date.
+
+    Returns
+    -------
+    df_full : pd.DataFrame
+        Processed full data set
+    df_extract : pd.DataFrame
+        Processed extract data set
+    """
+
+    logger = logging.getLogger("SCE")
+
+    # Process raw data, create full data set and smaller extract
+    df_full, df_extract = process_sce(df_orig)
+
+    # --- Merge HH income ranks from ACS ---
+
+    if df_ranks is not None:
+
+        df_rank = merge_inc_rank(df_full, "Q47", df_ranks)
+        df_full = pd.concat((df_full, df_rank), axis=1)
+
+        df_rank = merge_inc_rank(df_extract, "hh_inc_bin", df_ranks)
+        df_extract = pd.concat((df_extract, df_rank), axis=1)
+
+    # --- Restrict sample by final date ---
+
+    if final_date is not None:
+        logger.info(f"Restricting sample to dates before and including {final_date}")
+        keep = df_full["date"] <= final_date
+        if (n := len(keep) - keep.sum()) > 0:
+            logger.warning(f"  {n:,d} observations are excluded from the full data set")
+        df_full = df_full[keep].copy()
+
+        keep = df_extract["date"] <= final_date
+        if (n := len(keep) - keep.sum()) > 0:
+            logger.warning(f"  {n:,d} observations are excluded from the extract")
+        df_extract = df_extract[keep].copy()
+
+    return df_full, df_extract
 
 
 def main(econf: EnvConfig):
@@ -46,33 +105,38 @@ def main(econf: EnvConfig):
             df = pd.read_pickle(fn_cache)
         else:
             logger.info(f"Reading in {path}")
-            # Skip first line which contains the license terms
+            # Skip the first line which contains the license terms
             df = pd.read_excel(path, skiprows=1)
             df.to_pickle(fn_cache)
+
+        logger.info(f"  Initial intervew date: {df['survey_date'].dt.date.min()}")
+        logger.info(f"  Final intervew date:   {df['survey_date'].dt.date.max()}")
 
         df_all.append(df)
 
     df_orig = pd.concat(df_all, axis=0)
     df_orig = df_orig.sort_values(by=[VARNAME_ID, "date"]).reset_index(drop=True)
 
-    # --- Tabulate distribution of spell lengths ---
-    df_obs = df_orig.groupby(VARNAME_ID).size().value_counts().sort_index()
-    s = df_obs.to_string(header=True)
-    s = s.replace("\n", "\n\t")
-    logger.info(f"Distribution of spell lengths: \n\t{s}")
-
-    df_full, df_extract = process_sce(df_orig)
-
-    # --- Merge HH income ranks from ACS ---
+    # --- Load income rank data from ACS ---
 
     fn = os.path.join(econf.repodir, "data", "IPUMS_ftotinc_rank_by_year_sce_bins.csv")
     df_ranks = pd.read_csv(fn)
 
-    df_rank = merge_inc_rank(df_full, "Q47", df_ranks)
-    df_full = pd.concat((df_full, df_rank), axis=1)
+    # --- Process SCE data ---
 
-    df_rank = merge_inc_rank(df_extract, "hh_inc_bin", df_ranks)
-    df_extract = pd.concat((df_extract, df_rank), axis=1)
+    # The Fed NY silently updates the current file, so there is no way to get exactly
+    # the same sample as earlier. Impose some terminal date to keep the sample period
+    # the same.
+    final_date = pd.to_datetime("2024-10-01")
+
+    df_full, df_extract = process_data(df_orig, df_ranks, final_date)
+
+    # --- Tabulate distribution of spell lengths ---
+
+    df_obs = df_orig.groupby(VARNAME_ID).size().value_counts().sort_index()
+    s = df_obs.to_string(header=True)
+    s = s.replace("\n", "\n\t")
+    logger.info(f"Distribution of spell lengths: \n\t{s}")
 
     # --- Store results ---
 
